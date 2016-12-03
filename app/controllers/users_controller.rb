c83930359f1674906404e1e1e4fddad9f94a688f
sako kazuhiro <sako@rio.ne.jp>
2016-12-03 23:57:00 +0900

    thanks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
   def mail_send
     @mail = NoticeMailer.sendmail_confirm(application_params).deliver
-    logger.debug('aaaaaa')
-    render :text => '<body style="font-size: 400%;
-      background-color: #000000;
-      color: #C0C0C0;
-      height: 150px;
-      font-family: "MS 明朝";">
-      お問い合わせいただきありがとうございました。<br>
-      改めて担当者よりご連絡いたします。
-      </body>'
+    render :action => index
+    # render :text => '<body style="font-size: 400%;
+    #   background-color: #000000;
+    #   color: #C0C0C0;
+    #   height: 150px;
+    #   font-family: "MS 明朝";">
+    #   お問い合わせいただきありがとうございました。<br>
+    #   改めて担当者よりご連絡いたします。
+    #   <div align="center">
+    #     <div class="footer">
+    #       <table>
+    #         <tr>
+    #           <td><%= link_to 'TOP', items_path %></td>
+    #         </tr>
+    #       </table>
+    #     </div>
+    #   </body>'
   end
 
   def index
